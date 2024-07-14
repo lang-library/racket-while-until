c0c1d30cc4e0d7686d864666870f991596dd1e7e
JavaCommons Technologies <javacommons@gmail.com>
2024-07-14 18:34:44 +0900

    WhileUntil-v2024.0714.1834.43

--- info.rkt
+++ info.rkt
@@ -1,6 +1,15 @@
 #lang info
 (define collection "while-until")
+(define name "while-until: While/Until loops for Racket")
+(define categories '(devtools))
+(define can-be-loaded-with 'all)
+(define required-core-version "5.1.1")
+(define version "1.0")
+(define repositories '("4.x"))
 (define scribblings '(("while-until.scrbl")))
+(define primary-file "main.rkt")
+(define blurb
+  '("While/Until loops for Racket."))
 (define deps '("base"
                "compatibility-lib"
                "pprint-all"
@@ -8,3 +17,5 @@
                ))
 (define build-deps '("racket-doc"
                      "scribble-lib"))
+(define release-notes
+  '((p "First release")))
